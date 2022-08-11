9f4953e5f209de3b9ddd0910c33a67ddb7a9d542
Arti Kumari <artijha05@gmail.com>
2022-08-11 08:05:27 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import Form from "./Form";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>User Registration Form</h1>
+     <Form />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -2,25 +2,56 @@ import { useState } from "react";
 
 export default function Form() {
   const [fname, setFname] = useState("");
+   const [lname, setLname] = useState("");
+   
+   let user = {
+       fname: '',
+       lname: ''
+   }
 
-  const handleFnameChange = (e) => {
-    if (fname) {
-      alert("already firstName entered");
-    }
-
-    setFname(e.target.value);
+  const handleFnameChange = (e) => { 
+        setFname(e.target.value);
   };
+  
+   const handlelnameChange = (e) => {   
+        setLname(e.target.value);  
+  };
+  
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      if(user.fname == fname && user.lname === lname){
+          alert("user already created");
+      }else{
+          user = {
+              fname: fname,
+              lname: lname
+          }
+          
+          alert("user created succesfully");
+      }
+  }
 
   return (
     <div className="App">
       <form>
         <div>
-          <label>First Name</label>
+          <label for="fname">First Name</label>
           <input
             type="text"
             id="fname"
             onChange={(e) => handleFnameChange(e)}
           />
+        </div>
+         <div>
+          <label for="lname">Last Name</label>
+          <input
+            type="text"
+            id="lname"
+            onChange={(e) => handlelnameChange(e)}
+          />
+        </div>
+        <div>
+            <button onClick={(e) => handleSubmit(e)}>Submit</button>
         </div>
       </form>
     </div>
